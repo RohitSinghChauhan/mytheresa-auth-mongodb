5dfe0ad538c9d1be6c458443fad0ed6f1eda2f91
Rohit Singh <kailash.jc.rs@gmail.com>
2023-03-25 05:57:30 +0530

    updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,16 +8,12 @@ userRoute.post('/signup', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const alreadyExistingUser = await UserModel.findOne({ email });
-        if (alreadyExistingUser?.email) {
-            res.send({ 'msg': 'user exists already' });
-        }
-        else {
-            bcrypt.hash(password, 5, async function (err, hash) {
-                await UserModel.create({ email, password: hash });
-                res.send({ 'msg': 'User signed up successfully' });
-            })
-        }
+
+        bcrypt.hash(password, 5, async function (err, hash) {
+            await UserModel.create({ email, password: hash });
+            res.send({ 'msg': 'User signed up successfully' });
+        })
+
     }
     catch (err) {
         console.log('Something went wrong', err);
